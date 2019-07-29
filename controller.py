2b1a20ff7651e9937287381e0b640d28f4f32661
unknown <jakepetersen1221@gmail.com>
2019-07-29 16:37:30 -0700

    updated view

--- controller.py
+++ controller.py
@@ -1,17 +1,16 @@
 from model import model
-from view import view, buttons
+from view import view
 import numpy as np
 
 class controller():
     def __init__(self, root):
         self.model = model()
-        self.view1 = view(root)
-        self.view2 = buttons(self.view1, self.model.color) 
-        self.view2.bw.config(command=self.white_pressed)
-        self.view2.bb.config(command=self.black_pressed)
+        self.view = view(root, self.model.color)
+        self.view.bw.config(command=self.white_pressed)
+        self.view.bb.config(command=self.black_pressed)
 
     def color_changed(self, color):
-        self.view2.set_color(color)
+        self.view.set_color(color)
 
     def white_pressed(self):
         self.color_changed(self.model.recieve_data([1, 0]))
--- view.py
+++ view.py
@@ -2,14 +2,11 @@ import tkinter as tk
 from colormap import rgb2hex
 
 class view(tk.Toplevel):
-    def __init__(self, master): 
-        tk.Toplevel.__init__(self, master)
-
-        c = tk.Canvas(self, bg='black', height=500, width=600).pack()
-
-class buttons(tk.Toplevel):
     def __init__(self, master, color): 
         tk.Toplevel.__init__(self, master)
+
+        c = tk.Canvas(self, bg='black', height=500, width=600)
+
         self.buttonframe = tk.Frame(self)
         font = ('Helvetica', '30')
         self.hex = rgb2hex(color[0], color[1], color[2])
@@ -17,6 +14,7 @@ class buttons(tk.Toplevel):
         self.bw.grid(row=0, column=0)
         self.bb = tk.Button(self.buttonframe, width=10, height=2, bg=self.hex, fg='black', text='black', font=font)
         self.bb.grid(row=0, column=1)
+        c.pack()
         self.buttonframe.pack()
 
     def set_color(self, color):
